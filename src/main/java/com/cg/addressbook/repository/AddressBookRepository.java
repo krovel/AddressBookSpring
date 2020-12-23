568cd2aedefbcfd24eec61d0c8a1de50e4a23737
krove <krove@DESKTOP-Q5SED1R>
2020-12-24 03:43:44 +0530

    [Kashif]Added: Lombok Library to auto generate getters and setters

---
+++ src/main/java/com/cg/addressbook/repository/AddressBookRepository.java
@@ -0,0 +1,11 @@
+package com.cg.addressbook.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.addressbook.model.AddressBookData;
+
+@Repository
+public interface AddressBookRepository extends CrudRepository<AddressBookData, Integer> {
+
+}
